e02e1529b4156e57dcbec1c0adebb1b7fd0a0e7f
Jan Švábík <30616446+jansvabik@users.noreply.github.com>
2020-11-05 05:43:26 +0100

    Update time.sh

--- curl/time.sh
+++ curl/time.sh
@@ -1,2 +1,3 @@
 # get time of the http(s) request
+# run by: bash time.sh https://noltio.com/
 curl -w "DNS:\t\t%{time_namelookup}s\nCONN:\t\t%{time_connect}s\nTLS:\t\t%{time_appconnect}s\nPRETRANSFER:\t%{time_pretransfer}s\nREDIR:\t\t%{time_redirect}s\nTTFB:\t\t%{time_starttransfer}s\n-------------------------\nTOTAL TIME:\t%{time_total}s\n" -o /dev/null -s "$1"
